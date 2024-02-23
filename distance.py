cd4a2fd526d6e3726bcdaac0be71845263a6f16c
IrinaMoshik <moshikira27@gmail.com>
2024-02-23 22:32:44 +0200

    Created distance module

---
+++ distance.py
@@ -0,0 +1,13 @@
+def feet_to_meters(feet):
+    """
+    Convert feet to meters using the formula: meters = feet * 0.3048
+    """
+    meters = feet * 0.3048
+    return meters
+
+def meters_to_feet(meters):
+    """
+    Convert meters to feet using the formula: feet = meters / 0.3048
+    """
+    feet = meters / 0.3048
+    return feet
